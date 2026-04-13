648d165b1b1df3f31306ac7cf059cd7110309b6a
NavneetKumar-GS <navneetkumarbiz@gmail.com>
2026-04-13 15:52:28 +0530

    JavaScript

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+console.log("NAavneetttt")
+
+let temp = 30
+if(temp>20){
+    console.log("yayayay")
+}
+
+
+
+
+
+// const element = document.querySelector('h1');
+
+
+// element.style.backgroundColor = "red";
